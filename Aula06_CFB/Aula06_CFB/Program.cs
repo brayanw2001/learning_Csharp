2cbd15182bcc82f2782398bb2ed8822bbae8f167
BrayanWS <brayanws.2001@gmail.com>
2024-04-13 19:41:01 -0300

    primeiro commit

---
+++ Aula06_CFB/Aula06_CFB/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aula06_CFB
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n1 = 5, n2 = 6, n3 = 7;
+
+            Console.WriteLine("N1: {0} \nN2: {1} \nN3: {2}", n1, n2, n3);
+            Console.Read();
+        }
+    }
+}
---
+++ loops/loops/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace loops
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+            float[] notes = new float[4];
+            float res = 0;
+            int i;
+            Console.WriteLine("=== Sistema de Notas ===");
+
+            for (i = 0; i < notes.Length; i++)
+            {
+               Console.Write($"Insira a nota {i+1}: ");
+               notes[i] = float.Parse(Console.ReadLine());
+               res += notes[i];
+            }
+
+            Console.WriteLine("\nAs notas inseridas foram: ");
+
+            foreach (float n in notes)
+            {
+                Console.WriteLine(n);
+            }
+
+            res = res / i;
+            Console.WriteLine($"\nA média final foi: {res}");
+
+            if (res < 6)
+            {
+                Console.WriteLine("Reprovado.");
+            } 
+            else
+            {
+                Console.WriteLine("Aprovado");
+            }
+            Console.ReadLine();
+        }
+    }
+}
