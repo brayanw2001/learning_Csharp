c00e64bf7f512773e7a03d4f3b06e9c04a875cf9
Brayan <brayanws.2001@gmail.com>
2025-01-27 21:51:38 -0300

    feat: gerador de inputs para yaml esphome

---
+++ GeradorBinarySensor/GeradorBinarySensor/GeradorDeInputs.cs
@@ -0,0 +1,68 @@
+﻿
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorBinarySensor
+{
+    public class GeradorDeInputs
+    {
+
+        List<string> user_input = new List<string>();
+
+        public void PreencheIds()
+        {
+            bool opt = true;
+            System.Console.WriteLine("Insira, em sequência, os nomes das entidades (!q para sair)\n");
+
+            while (opt) 
+            { 
+                System.Console.Write("> ");
+                string input = Console.ReadLine();
+
+                if (input == "!q")
+                {  
+                    opt = false;
+                    break;
+                }
+
+                user_input.Add(input);
+            } 
+        }
+
+        public void ShowInputs()
+        {
+            int i = 0;
+            int j = 16;
+            int offset = -1;
+
+            Console.Clear();
+            Console.WriteLine("binary_sensor:");
+            Data data = new Data(); 
+                
+            foreach (string input in user_input)
+            {
+                Console.WriteLine(
+                    $"  - platform: gpio\n" +
+                    $"    name: {input}\n"  +
+                    $"    id: M1_INPUT_{data.idcInputs[i]}\n" +
+                    $"    pin\n" +
+                    $"      mcp23xxx:\n"+
+                    $"      number: {j} \n" +
+                    $"      mode: INPUT\n" +
+                    $"      inverted: True\n"  
+                );
+                if (data.idcInputs[i].Contains("E1"))
+                {
+                    offset *= -1;
+                }
+                i++;
+                j += offset;
+            }
+        }
+    }
+}
---
+++ GeradorBinarySensor/GeradorBinarySensor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorBinarySensor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GeradorDeInputs gerador = new GeradorDeInputs();
+            gerador.PreencheIds();
+            gerador.ShowInputs();
+        }
+    }
+}
