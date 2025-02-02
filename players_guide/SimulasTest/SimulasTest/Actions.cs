5025b43619849a83f3feb99408bf2f3a9ff76235
Brayan <brayanws.2001@gmail.com>
2025-02-02 14:20:07 -0300

    exercício de ENUM

---
+++ players_guide/SimulasTest/SimulasTest/Actions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulasTest
+{
+    internal class Actions
+    {
+        States state = States.locked;
+        public Actions()
+        {
+            while (true)
+            {
+                string text = $"The chest is {state}. What do you want to do? ";
+                string? answer = States.locked.ToString();
+
+                switch (state)
+                {
+                    case States.locked:
+
+                        Console.WriteLine(text);
+                        answer = Console.ReadLine();
+
+                        if (answer == "unlock")
+                            toUnlock();
+                        else
+                            Console.WriteLine("Tente novamente");
+                        break;
+
+                    case States.closed:
+
+                        Console.WriteLine(text);
+                        answer = Console.ReadLine();
+
+                        if (answer == "open")
+                            toOpen();
+                        else if (answer == "lock")
+                            toLock();
+                        else
+                            Console.WriteLine("Tente novamente");
+                        break;
+
+                    case States.open:
+
+                        Console.WriteLine(text);
+                        answer = Console.ReadLine();
+
+                        if (answer == "close")
+                            toClose();
+                        else
+                            Console.WriteLine("Tente novamente");
+                        break;
+                }
+            }
+        }
+
+        internal States toClose() => state = States.closed;
+        internal States toLock() => state = States.locked;
+        internal States toUnlock() => state = States.closed;
+        internal States toOpen() => state = States.open;
+    }
+}
---
+++ players_guide/SimulasTest/SimulasTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimulasTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Actions actions = new Actions();
+        }
+    }
+}
