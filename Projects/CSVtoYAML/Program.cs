ac2f69c8664ad2c42e512ae144eaff61bfdfc724
BrayanWS <brayanws.2001@protonmail.com>
2025-02-27 12:14:28 -0300

    Criação CSVtoYAML

---
+++ Projects/CSVtoYAML/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CsvHelper;
+using System.IO;
+using System.Globalization;
+using System.Linq;
+namespace CSVtoYAML;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var StreamReader = new StreamReader(@"/home/brayan/Documentos/CSVs_mapeamentos/101.csv"))
+        {
+            using (var csvReader = new CsvReader(StreamReader, CultureInfo.InvariantCulture))
+            {
+
+            }
+        }
+
+    }
+}
