3333d21eb93ee4e5122fb522090532dbb0e7b3b3
Brayan <brayanws.2001@gmail.com>
2025-03-05 20:00:27 -0300

    leitura das colunas em suas respectiveis variaveis

---
+++ Projects/CSVtoYAML/IOs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+namespace CSVtoYAML
+{
+    public class IOs
+    {
+        [Name("Mod Entrada")]
+        public string modEntrada { get; set; }
+        [Name("Area Entrada")]
+        public string areaEntrada { get; set; }
+
+        [Name("Mod Saida")]
+        public string modSaida { get; set; }
+        [Name("Area Saida")]
+        public string areaSaida { get; set; }
+        [Name("Circuito")]
+        public string circuito { get; set; }
+    }
+}
--- Projects/CSVtoYAML/Program.cs
+++ Projects/CSVtoYAML/Program.cs
@@ -3,19 +3,21 @@ using CsvHelper;
 using System.IO;
 using System.Globalization;
 using System.Linq;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
 namespace CSVtoYAML;
 
 class Program
 {
     static void Main(string[] args)
     {
-        using (var StreamReader = new StreamReader(@"/home/brayan/Documentos/CSVs_mapeamentos/101.csv"))
+        //using (var StreamReader = new StreamReader(@"/home/brayan/Documentos/CSVs_mapeamentos/101.csv"))
+        using (var StreamReader = new StreamReader(@"C:\Users\braya\OneDrive\Documentos\Projects\UNISEC\Empreendimentos\Chateau Meirelles\101 (2).csv"))
         {
             using (var csvReader = new CsvReader(StreamReader, CultureInfo.InvariantCulture))
             {
-
+                var records = csvReader.GetRecords<IOs>().ToList();
             }
         }
-
     }
 }
