fa11def3c5bac312795a9f33dde2b2b34bb91835
Brayan <brayanws.2001@gmail.com>
2025-03-08 00:02:24 -0300

    correção na indentação do arquivo de saída

--- Projects/CSVtoYAML/YamlConstructor.cs
+++ Projects/CSVtoYAML/YamlConstructor.cs
@@ -26,7 +26,7 @@ namespace CSVtoYAML
         public void BinarySensorGenerator()
         {
             IOsNumbers number = new IOsNumbers();
-            Console.WriteLine($"  - platform: gpio\r\n    name: {areaEntrada}\r\n    id: {modEntrada.Replace(" ", "_INPUT_")}\r\n    pin:\r\n      mcp23xxx: IDC_M1_MCP4_I2CA_IN\r\n      number: {number.ReturnPinNumber(modEntrada)}\r\n      mode: INPUT\r\n      inverted: True\r\n    on_multi_click:\r\n    - timing:\r\n       - ON for at least 100ms\r\n      then:\r\n        - light.toggle: {modSaida.Replace(" ", "_INPUT_")}\n");
+            Console.WriteLine($"  - platform: gpio\r\n    name: {areaEntrada}\r\n    id: {modEntrada.Replace(" ", "_INPUT_")}\r\n    pin:\r\n      mcp23xxx: IDC_M1_MCP4_I2CA_IN\r\n      number: {number.ReturnPinNumber(modEntrada)}\r\n      mode: INPUT\r\n      inverted: True\r\n    on_multi_click:\r\n      - timing:\r\n        - ON for at least 100ms\r\n        then:\r\n          - light.toggle: {modSaida.Replace(" ", "_INPUT_")}\n");
         }
 
         public void OutputGenerator()
