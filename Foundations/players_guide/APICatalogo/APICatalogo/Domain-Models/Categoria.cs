a95b8b5b51447f6a2cb80c3177222db02e403be3
Brayan <brayanws.2001@gmail.com>
2025-07-06 18:32:39 -0300

    refazendo o projeto

---
+++ Foundations/players_guide/APICatalogo/APICatalogo/Domain-Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace APICatalogo.Domain_Models;
+
+public class Categoria
+{
+    public int CategoriaID { get; set; }    // chave primária
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+}
