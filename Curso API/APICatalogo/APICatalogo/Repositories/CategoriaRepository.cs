1243f77617bf387c58cded58958afdc670e3e7d8
Brayan <brayanws.2001@gmail.com>
2025-09-27 14:48:03 -0300

    feat: criação de padrão repository para categoria

---
+++ Curso API/APICatalogo/APICatalogo/Repositories/CategoriaRepository.cs
@@ -0,0 +1,72 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Domain_Models;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly AppDbContext _context;
+
+        CategoriaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Categoria> GetCategorias()
+        {
+           return _context.Categorias.ToList();
+        }
+
+        public Categoria GetCategoriaId(int id)
+        {
+            return _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+        }
+
+        public Categoria CreateCategoria(Categoria categoria)
+        {
+            if (categoria == null)
+            {
+                throw new ArgumentNullException(nameof(categoria));
+            }
+            else
+            {
+                _context.Categorias.Add(categoria);
+                _context.SaveChanges();
+                return categoria;
+            }
+        }
+
+        public Categoria Update(Categoria categoria)
+        {
+            if (categoria == null)
+            {
+                throw new ArgumentNullException(nameof(categoria));
+            }
+            else
+            {
+                _context.Entry(categoria).State = EntityState.Modified;
+                _context.SaveChanges();
+                return categoria;
+            }
+        }
+
+        public Categoria Delete(int id)
+        {
+           var categoria = _context.Categorias.Find(id);
+           
+            if (categoria == null)
+            {
+                throw new ArgumentNullException(nameof(categoria));
+            }
+            else
+            {
+                _context.Remove(categoria);
+                _context.SaveChanges();
+                return categoria;
+            }
+        }
+
+    }
+}
---
+++ Curso API/APICatalogo/APICatalogo/Repositories/ICategoriaRepository.cs
@@ -0,0 +1,13 @@
+﻿using APICatalogo.Domain_Models;
+
+namespace APICatalogo.Repositories
+{
+    public interface ICategoriaRepository
+    {
+        IEnumerable<Categoria>GetCategorias();
+        Categoria GetCategoriaId(int id);
+        Categoria CreateCategoria(Categoria categoria);
+        Categoria Update(Categoria categoria);
+        Categoria Delete(int id);
+    }
+}
